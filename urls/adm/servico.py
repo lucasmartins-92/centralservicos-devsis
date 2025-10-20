4dfdbb2f9953b91e0f91d21f9049d5f319885dae
lucasmartins-92 <lucas.mart19@gmail.com>
2025-10-20 20:19:56 -0300

    add CRUD-delete (aula 10)

--- urls/adm/servico.py
+++ urls/adm/servico.py
@@ -3,26 +3,28 @@ from flask import Blueprint, render_template, request
 from database.setor_dao import SetorDAO
 from database.servico_dao import ServicoDAO
 
-bp_serv = Blueprint('serv', __name__, url_prefix='/adm/serv')
+bp_serv = Blueprint("serv", __name__, url_prefix="/adm/serv")
 
 
-@bp_serv.route('/incluir')  # /adm/serv/incluir
+@bp_serv.route("/incluir")  # /adm/serv/incluir
 def incluir():
     dao = SetorDAO()
-    lst_setores = dao.read_by_filters([('sts_setor', '=', 'A')])
-    return render_template('adm/serv/incluir.html', msg="", css_msg="", lst_setores=lst_setores)
+    lst_setores = dao.read_by_filters([("sts_setor", "=", "A")])
+    return render_template(
+        "adm/serv/incluir.html", msg="", css_msg="", lst_setores=lst_setores
+    )
 
 
-@bp_serv.route('/salvar_incluir', methods=['POST'])  # /adm/serv/incluir
+@bp_serv.route("/salvar_incluir", methods=["POST"])  # /adm/serv/incluir
 def salvar_incluir():
     dao = ServicoDAO()
     serv = dao.new_object()
-    serv.nme_servico = request.form['nme_servico']
-    serv.num_dias_servico = request.form['num_dias_servico']
-    serv.vlr_servico = request.form['vlr_servico']
-    serv.txt_modelo_servico = request.form['txt_modelo_servico']
-    serv.sts_servico = request.form['sts_servico']
-    serv.cod_setor = request.form['cod_setor']
+    serv.nme_servico = request.form["nme_servico"]
+    serv.num_dias_servico = request.form["num_dias_servico"]
+    serv.vlr_servico = request.form["vlr_servico"]
+    serv.txt_modelo_servico = request.form["txt_modelo_servico"]
+    serv.sts_servico = request.form["sts_servico"]
+    serv.cod_setor = request.form["cod_setor"]
 
     if dao.insert(serv):
         msg = f"Serviço número {serv.idt_servico} inserido com sucesso!"
@@ -32,32 +34,89 @@ def salvar_incluir():
         css_msg = "erro"
 
     dao_setor = SetorDAO()
-    lst_setores = dao_setor.read_by_filters([('sts_setor', '=', 'A')])
+    lst_setores = dao_setor.read_by_filters([("sts_setor", "=", "A")])
 
-    return render_template('adm/serv/incluir.html', msg=msg, css_msg=css_msg, lst_setores=lst_setores)
+    return render_template(
+        "adm/serv/incluir.html", msg=msg, css_msg=css_msg, lst_setores=lst_setores
+    )
 
 
-@bp_serv.route('/consultar')  # /adm/serv/consultar
+@bp_serv.route("/consultar")  # /adm/serv/consultar
 def consultar():
     dao_setor = SetorDAO()
-    setores = dao_setor.read_by_filters([('sts_setor', '=', 'A')])
-    return render_template('adm/serv/consultar.html', servicos=[], setores=setores, filtro_usado='')
+    setores = dao_setor.read_by_filters([("sts_setor", "=", "A")])
+    return render_template(
+        "adm/serv/consultar.html", servicos=[], setores=setores, filtro_usado=""
+    )
 
 
-@bp_serv.route('/roda_consultar', methods=['POST'])  # /adm/serv/rodar_consultar
+@bp_serv.route("/roda_consultar", methods=["POST"])  # /adm/serv/rodar_consultar
 def roda_consultar():
-    nme_servico = request.form['nme_servico']
-    cod_setor = request.form['cod_setor']
+    nme_servico = request.form["nme_servico"]
+    cod_setor = request.form["cod_setor"]
     filtros = []
     if nme_servico:
-        filtros.append(('nme_servico', 'ilike', f'%{nme_servico}%'))
+        filtros.append(("nme_servico", "ilike", f"%{nme_servico}%"))
     if cod_setor:
-        filtros.append(('cod_setor', '=', int(cod_setor)))
+        filtros.append(("cod_setor", "=", int(cod_setor)))
     filtro_usado = f'Nome do Serviço: {nme_servico or "Não informado"} / Código do Setor: {cod_setor or "Todos"}'
 
     dao = ServicoDAO()
     servicos = dao.read_by_filters(filtros)
 
     dao_setor = SetorDAO()
-    setores = dao_setor.read_by_filters([('sts_setor', '=', 'A')])
-    return render_template('adm/serv/consultar.html', servicos=servicos, setores=setores, filtro_usado=filtro_usado)
+    setores = dao_setor.read_by_filters([("sts_setor", "=", "A")])
+    return render_template(
+        "adm/serv/consultar.html",
+        servicos=servicos,
+        setores=setores,
+        filtro_usado=filtro_usado,
+    )
+
+
+@bp_serv.route("/atualizar")  # /adm/serv/atualizar
+def atualizar():
+    dao_setor = SetorDAO()
+    setores = dao_setor.read_by_filters([("sts_setor", "=", "A")])
+    return render_template(
+        "adm/serv/atualizar.html", servicos=[], setores=setores, filtro_usado=""
+    )
+
+
+@bp_serv.route("/roda_atualizar", methods=["POST"])  # /adm/serv/rodar_atualizar
+def roda_atualizar():
+    nme_servico = request.form["nme_servico"]
+    cod_setor = request.form["cod_setor"]
+    filtros = []
+    if nme_servico:
+        filtros.append(("nme_servico", "ilike", f"%{nme_servico}%"))
+    if cod_setor:
+        filtros.append(("cod_setor", "=", int(cod_setor)))
+    filtro_usado = f'Nome do Serviço: {nme_servico or "Não informado"} / Código do Setor: {cod_setor or "Todos"}'
+
+    dao = ServicoDAO()
+    servicos = dao.read_by_filters(filtros)
+
+    dao_setor = SetorDAO()
+    setores = dao_setor.read_by_filters([("sts_setor", "=", "A")])
+    return render_template(
+        "adm/serv/atualizar.html",
+        servicos=servicos,
+        setores=setores,
+        filtro_usado=filtro_usado,
+    )
+
+
+@bp_serv.route("/excluir/<int:idt>")
+def excluir(idt):
+    dao = ServicoDAO()
+    if dao.delete(idt):
+        msg = "Serviço excluído com sucesso!"
+        css_msg = "sucesso"
+    else:
+        msg = "Falha ao tentar excluir serviço! Verifique se existe alguma dependência!"
+        css_msg = "erro"
+    setor = dao.read_by_idt(idt)
+    return render_template(
+        "adm/serv/atualizar.html", msg=msg, css_msg=css_msg, setores=[], filtro_usado=""
+    )
--- urls/adm/setor.py
+++ urls/adm/setor.py
@@ -1,22 +1,24 @@
 from flask import Blueprint, render_template, request
 from database.setor_dao import SetorDAO
 
-bp_setor = Blueprint('setor', __name__, template_folder="templates", url_prefix='/adm/setor')
+bp_setor = Blueprint(
+    "setor", __name__, template_folder="templates", url_prefix="/adm/setor"
+)
 
 
-@bp_setor.route('/incluir')  # /adm/setor/incluir
+@bp_setor.route("/incluir")  # /adm/setor/incluir
 def incluir():
-    return render_template('adm/setor/incluir.html', msg="", css_msg="")
+    return render_template("adm/setor/incluir.html", msg="", css_msg="")
 
 
-@bp_setor.route('/salvar_incluir', methods=['POST'])  # /adm/setor/incluir
+@bp_setor.route("/salvar_incluir", methods=["POST"])  # /adm/setor/incluir
 def salvar_incluir():
     dao = SetorDAO()
     setor = dao.new_object()
-    setor.sgl_setor = request.form['sgl_setor']
-    setor.nme_setor = request.form['nme_setor']
-    setor.eml_setor = request.form['eml_setor']
-    setor.sts_setor = request.form['sts_setor']
+    setor.sgl_setor = request.form["sgl_setor"]
+    setor.nme_setor = request.form["nme_setor"]
+    setor.eml_setor = request.form["eml_setor"]
+    setor.sts_setor = request.form["sts_setor"]
     if dao.insert(setor):
         msg = f"Setor número {setor.idt_setor} inserido com sucesso!"
         css_msg = "sucesso"
@@ -24,18 +26,54 @@ def salvar_incluir():
         msg = "Erro ao tentar incluir setor!"
         css_msg = "erro"
 
-    return render_template('adm/setor/incluir.html', msg=msg, css_msg=css_msg)
+    return render_template("adm/setor/incluir.html", msg=msg, css_msg=css_msg)
 
 
-@bp_setor.route('/consultar')  # /adm/setor/consultar
+@bp_setor.route("/consultar")  # /adm/setor/consultar
 def consultar():
-    return render_template('adm/setor/consultar.html', setores=[], filtro_usado='')
+    return render_template("adm/setor/consultar.html", setores=[], filtro_usado="")
 
 
-@bp_setor.route('/roda_consultar', methods=['POST'])  # /adm/setor/roda_consultar
+@bp_setor.route("/roda_consultar", methods=["POST"])  # /adm/setor/roda_consultar
 def roda_consultar():
-    nme_setor = request.form['nme_setor']
-    filtro_usado = f'Nome do Setor: {nme_setor}'
+    nme_setor = request.form["nme_setor"]
+    filtro_usado = f"Nome do Setor: {nme_setor}"
     dao = SetorDAO()
-    setores = dao.read_by_like('nme_setor', nme_setor)
-    return render_template('adm/setor/consultar.html', setores=setores, filtro_usado=filtro_usado)
+    setores = dao.read_by_like("nme_setor", nme_setor)
+    return render_template(
+        "adm/setor/consultar.html", setores=setores, filtro_usado=filtro_usado
+    )
+
+
+@bp_setor.route("/atualizar")  # /adm/setor/atualizar
+def atualizar():
+    return render_template("adm/setor/atualizar.html", setores=[], filtro_usado="")
+
+
+@bp_setor.route("/roda_atualizar", methods=["POST"])  # /adm/setor/rodar_atualizar
+def roda_atualizar():
+    nme_setor = request.form["nme_setor"]
+    filtro_usado = f"Nome do Setor: {nme_setor}"
+    dao = SetorDAO()
+    setores = dao.read_by_like("nme_setor", nme_setor)
+    return render_template(
+        "adm/setor/atualizar.html", setores=setores, filtro_usado=filtro_usado
+    )
+
+
+@bp_setor.route("/excluir/<int:idt>")
+def excluir(idt):
+    dao = SetorDAO()
+    if dao.delete(idt):
+        msg = "Setor excluído com sucesso!"
+        css_msg = "sucesso"
+    else:
+        msg = "Falha ao tentar excluir setor! Verifique se existe alguma dependência!"
+        css_msg = "erro"
+    return render_template(
+        "adm/setor/atualizar.html",
+        msg=msg,
+        css_msg=css_msg,
+        setores=[],
+        filtro_usado="",
+    )
