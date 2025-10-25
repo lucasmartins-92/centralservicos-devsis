1842cb5a6ae0528c923102555092af479875b191
lucasmartins-92 <lucas.mart19@gmail.com>
2025-10-25 19:16:25 -0300

    add: update e delete de tipo_ocorrencia

--- urls/adm/tipo_ocorrencia.py
+++ urls/adm/tipo_ocorrencia.py
@@ -45,3 +45,43 @@ def roda_consultar():
 
     return render_template('adm/tipo_ocorrencia/consultar.html', tipos_ocorrencia=tipos_ocorrencia,
                            filtro_usado=filtro_usado)
+
+
+@bp_tipo_ocorrencia.route('/atualizar')  # /adm/tipo_ocorrencia/atualizar
+def atualizar():
+    return render_template('adm/tipo_ocorrencia/atualizar.html', tipos_ocorrencia=[], filtro_usado='', msg='', css_msg='')
+
+
+@bp_tipo_ocorrencia.route('/roda_atualizar', methods=['POST'])  # /adm/tipo_ocorrencia/roda_atualizar
+def roda_atualizar():
+    nme_tipo_ocorrencia = request.form['nme_tipo_ocorrencia']
+    filtro_usado = f"Nome do Tipo de Ocorrência: {nme_tipo_ocorrencia}"
+
+    dao = TipoOcorrenciaDAO()
+    tipos_ocorrencia = dao.read_by_like('nme_tipo_ocorrencia', nme_tipo_ocorrencia)
+
+    return render_template('adm/tipo_ocorrencia/atualizar.html', tipos_ocorrencia=tipos_ocorrencia,
+                           filtro_usado=filtro_usado)
+
+
+@bp_tipo_ocorrencia.route('/excluir/<int:idt>')
+def excluir(idt):
+    dao = TipoOcorrenciaDAO()
+    if dao.delete(idt):
+        msg = 'Tipo de ocorrência excluído com sucesso!'
+        css_msg = 'sucesso'
+    else:
+        msg = 'Falha ao tentar excluir tipo de ocorrência! Verifique se existe alguma dependência!'
+        css_msg = 'erro'
+    return render_template('adm/tipo_ocorrencia/atualizar.html', msg=msg, css_msg=css_msg,
+                           tipos_ocorrencia=[], filtro_usado='')
+
+
+@bp_tipo_ocorrencia.route('/alterar/<int:idt>')
+def alterar(idt):
+    dao = TipoOcorrenciaDAO()
+    tipo = dao.read_by_idt(idt)
+    if not tipo:
+        return render_template('adm/tipo_ocorrencia/atualizar.html', msg='Tipo de ocorrência não encontrado.',
+                               css_msg='erro', tipos_ocorrencia=[], filtro_usado='')
+    return render_template('adm/tipo_ocorrencia/alterar.html', tipo_ocorrencia=tipo, msg='', css_msg='')
